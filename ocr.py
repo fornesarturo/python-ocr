edde68d059b17647187ea53a83042e1df39f8c9e
fornesarturo <fornesarturo@gmail.com>
2017-10-14 14:19:29 -0500

    Now with significant comments

--- ocr.py
+++ ocr.py
@@ -20,14 +20,8 @@ args = vars(ap.parse_args())
 ref = cv2.imread(args["reference"])
 ref = imutils.resize(ref, width=100)
 
-#cv2.imshow("Raw", ref);
-#cv2.waitKey(0);
-
 ref = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
 ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY_INV)[1]
-
-#cv2.imshow("Gray", ref);
-#cv2.waitKey(0);
 
 
 # find contours in the OCR-A image (i.e,. the outlines of the digits)
@@ -66,7 +60,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
 # apply a tophat (whitehat) morphological operator to find light
-# regions against a dark background (i.e., the credit card numbers)
+# regions against a dark background (i.e., the text areas)
 tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
 #cv2.imshow("Tophat", tophat)
 #cv2.waitKey(0)
@@ -79,12 +73,10 @@ gradX = np.absolute(gradX)
 (minVal, maxVal) = (np.min(gradX), np.max(gradX))
 gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
 gradX = gradX.astype("uint8")
-#cv2.imshow("GradX", gradX)
-#cv2.waitKey(0)
 
 
 # apply a closing operation using the rectangular kernel to help
-# cloes gaps in between credit card number digits, then apply
+# cloes gaps in between letters, then apply
 # Otsu's thresholding method to binarize the image
 gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
 thresh = cv2.threshold(gradX, 0, 255,
@@ -100,49 +92,36 @@ thresh = cv2.threshold(gradX, 0, 255,
 # cv2.waitKey(0)
 
 # find contours in the thresholded image, then initialize the
-# list of digit locations
+# list of text locations
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 locs = []
 
-print(cnts)
-
 
 # loop over the contours
 for (i, c) in enumerate(cnts):
-	# compute the bounding box of the contour, then use the
-	# bounding box coordinates to derive the aspect ratio
     (x, y, w, h) = cv2.boundingRect(c)
-    ar = w / float(h)
 
-	# since credit cards used a fixed size fonts with 4 groups
-	# of 4 digits, we can prune potential contours based on the
-	# aspect ratio
-	# if ar > 2.5 and ar < 4.0:
-	# 	# contours can further be pruned on minimum/maximum width
-	# 	# and height
     if (w > 20) and (h > 10 and h < 30):
-	# 		# append the bounding box region of the digits group
-	# 		# to our locations list
         locs.append((x, y, w, h))
 
 
-# sort the digit locations from top-to-bottom, then initialize the
-# list of classified digits
+# sort the areas of text from top-to-bottom, then initialize the
+# list of classified text
 locs = sorted(locs, key=lambda x:x[1])
 output = []
 
 
 # loop over the 4 groupings of 4 digits
-print("Enumerate size: " + str((enumerate(locs))))
+"""
+Each rectangle (area of text)
+"""
 for (i, (gX, gY, gW, gH)) in enumerate(locs):
     # initialize the list of group digits
     groupOutput = []
 
-    # extract the group ROI of 4 digits from the grayscale image,
-    # then apply thresholding to segment the digits from the
-    # background of the credit card
+    # Extract the rectangle from the gray image
     group = gray[gY - 5:gY + gH + 5, gX - 5:gX + gW + 5]
     #cv2.imshow("group" + str(i), group)
     #cv2.waitKey(0)
@@ -151,14 +130,25 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
     cv2.imshow("group threshed" + str(i), group)
     cv2.waitKey(0)
 
-    # detect the contours of each individual digit in the group,
-    # then sort the digit contours from left to right
+
+    """
+    Each letter for every section
+    """
     digitCnts = cv2.findContours(group.copy(), cv2.RETR_LIST,
     	cv2.CHAIN_APPROX_SIMPLE)
     digitCnts = digitCnts[0] if imutils.is_cv2() else digitCnts[1]
-    digitCnts = contours.sort_contours(digitCnts,
+
+    # Get Contours that could be letters based on size
+    actualLetters = []
+    for (index, possibleLetter) in enumerate(digitCnts):
+        (x, y, w, h) = cv2.boundingRect(possibleLetter)
+        if (h > 5):
+            actualLetters.append(possibleLetter)
+
+    digitCnts = contours.sort_contours(actualLetters,
     	method="left-to-right")[0]
-        	# loop over the digit contours
+
+    # Try to match the letter to the template.
     for c in digitCnts:
         # Bounding Rectangle of an area found in the ID.
         (x, y, w, h) = cv2.boundingRect(c)
@@ -175,7 +165,7 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
         scores = []
 
 
-        # loop over the reference digit name and digit ROI
+        # loop over the reference name and ROI
         for (digit, digitROI) in digits.items():
         	# apply correlation-based template matching, take the
         	# score, and update the scores list
@@ -187,7 +177,9 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
         # the classification for the digit ROI will be the reference
         # digit name with the *largest* template matching score
         groupOutput.append(str(np.argmax(scores)))
-
+    """END
+    Each letter for every section
+    """
 
     # draw the digit classifications around the group
     cv2.rectangle(image, (gX - 5, gY - 5),
