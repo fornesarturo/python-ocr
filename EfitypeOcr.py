310b838716d50e42ed99b306de2f4d7f50fa12dd
fornesarturo <fornesarturo@gmail.com>
2017-10-14 14:46:51 -0500

    Efitype test

---
+++ EfitypeOcr.py
@@ -0,0 +1,52 @@
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=False,
+	help="path to input image")
+ap.add_argument("-r", "--reference", required=True,
+	help="path to reference OCR-A image")
+args = vars(ap.parse_args())
+
+
+# load the reference OCR-A image from disk, convert it to grayscale,
+# and threshold it, such that the digits appear as *white* on a
+# *black* background
+# and invert it, such that the digits appear as *white* on a *black*
+ref = cv2.imread(args["reference"])
+
+cv2.imshow("Raw", ref)
+cv2.waitKey(0)
+
+ref = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
+ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY_INV)[1]
+
+cv2.imshow("Gray", ref)
+cv2.waitKey(0)
+
+# find contours in the OCR-A image (i.e,. the outlines of the digits)
+# sort them from left to right, and initialize a dictionary to map
+# digit name to the ROI
+refCnts = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+refCnts = refCnts[0] if imutils.is_cv2() else refCnts[1]
+refCnts = contours.sort_contours(refCnts, method="left-to-right")[0]
+digits = {}
+
+
+# loop over the OCR-A reference contours
+for (i, c) in enumerate(refCnts):
+	# compute the bounding box for the digit, extract it, and resize
+	# it to a fixed size
+    (x, y, w, h) = cv2.boundingRect(c)
+    roi = ref[y:y + h, x:x + w]
+    cv2.imshow("roi [{}]".format(i), roi)
+    cv2.waitKey(0)
+    character = input("What character is this? ")
+    roi = cv2.resize(roi, (57, 88))
+	# update the digits dictionary, mapping the digit name to the ROI
+    digits[c] = roi
