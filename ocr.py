a776437a6175696d42cb044f53c4e97341c45919
Hermes Espínola <hermesespinola@gmail.com>
2017-10-16 07:20:34 -0500

    Add perspective transform method

--- ocr.py
+++ ocr.py
@@ -1,96 +1,28 @@
-from skimage.filters import threshold_local
-from transform import four_point_transform
 from imutils import contours
-from PIL import Image
-import pytesseract
 import numpy as np
 import argparse
 import imutils
 import cv2
-import os
-
-def imWarped(image, debug=False):
-	# load the image and compute the ratio of the old height
-	# to the new height, clone it, and resize it
-	ratio = image.shape[0] / 500.0
-	orig = image.copy()
-	image = imutils.resize(image, height = 500)
-
-	# convert the image to grayscale, blur it, and find edges
-	# in the image
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (5, 5), 0)
-	edged = cv2.Canny(gray, 75, 200)
-
-	# show the original image and the edge detected image
-	if debug:
-		print("STEP 1: Edge Detection")
-		cv2.imshow("Image", image)
-		cv2.imshow("Edged", edged)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-
-	# find the contours in the edged image, keeping only the
-	# largest ones, and initialize the screen contour
-	_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
-
-	# loop over the contours
-	for c in cnts:
-		# approximate the contour
-		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.1 * peri, True)
-
-		# if our approximated contour has four points, then we
-		# can assume that we have found our screen
-		if len(approx) == 4:
-			screenCnt = approx
-			break
-
-	# show the contour (outline) of the piece of paper
-	if debug:
-		print("STEP 2: Find contours of paper")
-		cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-		cv2.imshow("Outline", image)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-
-	# apply the four point transform to obtain a top-down
-	# view of the original image
-	warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
-
-	# convert the warped image to grayscale, then threshold it
-	# to give it that 'black and white' paper effect
-	# warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-	# warped = threshold_local(warped, 251, offset = 10)
-	# warped = warped.astype("uint8") * 255
-
-	# show the original and scanned images
-	if debug:
-		print("STEP 3: Apply perspective transform")
-		cv2.destroyAllWindows()
-		cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-	return warped
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
 ap.add_argument("-r", "--reference", required=False,
-	help="path to reference OCR-A image", default='./ocr-a.jpg')
+	help="path to reference OCR-A image")
 args = vars(ap.parse_args())
+
 
 # load the reference OCR-A image from disk, convert it to grayscale,
 # and threshold it, such that the digits appear as *white* on a
 # *black* background
 # and invert it, such that the digits appear as *white* on a *black*
 ref = cv2.imread(args["reference"])
-# ref = imutils.resize(ref, width=100)
+ref = imutils.resize(ref, width=100)
 
 ref = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
 ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY_INV)[1]
+
 
 # find contours in the OCR-A image (i.e,. the outlines of the digits)
 # sort them from left to right, and initialize a dictionary to map
@@ -100,6 +32,7 @@ refCnts = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL,
 refCnts = refCnts[0] if imutils.is_cv2() else refCnts[1]
 refCnts = contours.sort_contours(refCnts, method="top-to-bottom")[0]
 digits = {}
+
 
 # loop over the OCR-A reference contours
 for (i, c) in enumerate(refCnts):
@@ -120,17 +53,17 @@ sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 
 # load the input image, resize it, and convert it to grayscale
 image = cv2.imread(args["image"])
-image = imWarped(image)
 image = imutils.resize(image, width=600)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray", gray)
-cv2.waitKey(0)
+#cv2.imshow("Image", gray)
+#cv2.waitKey(0)
+
 
 # apply a tophat (whitehat) morphological operator to find light
 # regions against a dark background (i.e., the text areas)
 tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
-cv2.imshow("Tophat", tophat)
-cv2.waitKey(0)
+#cv2.imshow("Tophat", tophat)
+#cv2.waitKey(0)
 
 # compute the Scharr gradient of the tophat image, then scale
 # the rest back into the range [0, 255]
@@ -148,8 +81,8 @@ gradX = gradX.astype("uint8")
 gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
 thresh = cv2.threshold(gradX, 0, 255,
 	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-cv2.imshow("Thresh", thresh)
-cv2.waitKey(0)
+#cv2.imshow("Thresh", thresh)
+#cv2.waitKey(0)
 
 
 # apply a second closing operation to the binary image, again
@@ -179,33 +112,8 @@ for (i, c) in enumerate(cnts):
 locs = sorted(locs, key=lambda x:x[1])
 output = []
 
-# NAME_ROI = (187, 108, 256, 37)
-# ADDR_ROI = (187, 166, 277, 75)
-# CURP_ROI = (229, 262, 170, 26)
-# BRDY_ROI = (485, 102, 95, 20)
-# SEX_ROI = (558, 123, 23, 19)
-NAME_ROI = (186, 108, 255, 56)
-ADDR_ROI = (182, 179, 281, 61)
-CURP_ROI = (226, 265, 170, 20)
-BRDY_ROI = (487, 106, 81, 16)
-SEX_ROI = (524, 122, 64, 26)
 
-# Uncomment to recalculate ROIs from ID
-# fromCenter = False
-# NAME_ROI = cv2.selectROI("Image", tophat, fromCenter)
-# ADDR_ROI = cv2.selectROI("Image", tophat, fromCenter)
-# CURP_ROI = cv2.selectROI("Image", tophat, fromCenter)
-# BRDY_ROI = cv2.selectROI("Image", tophat, fromCenter)
-# SEX_ROI  = cv2.selectROI("Image", tophat, fromCenter)
-# print('NAME_ROI = ' + str(NAME_ROI))
-# print('ADDR_ROI = ' + str(ADDR_ROI))
-# print('CURP_ROI = ' + str(CURP_ROI))
-# print('BRDY_ROI = ' + str(BRDY_ROI))
-# print('SEX_ROI = ' + str(SEX_ROI))
-# exit()
-
-locs = [NAME_ROI, ADDR_ROI, CURP_ROI, BRDY_ROI, SEX_ROI]
-locs = sorted(locs, key=lambda x:x[1])
+# loop over the 4 groupings of 4 digits
 """
 Each rectangle (area of text)
 """
@@ -219,24 +127,71 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
     #cv2.waitKey(0)
     group = cv2.threshold(group, 0, 255,
     	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    # write the image to disk as a temporary file so we can
-    # apply OCR to it
-    filename = "{}.png".format(os.getpid())
-    cv2.imwrite(filename, group)
-
-    # load the image as a PIL/Pillow image, apply OCR, and then delete
-    # the temporary file
-    text = pytesseract.image_to_string(Image.open(filename), lang='spa.news-gothic-light')
-    text_default = pytesseract.image_to_string(Image.open(filename), lang='spa')
-    text_eng = pytesseract.image_to_string(Image.open(filename))
-    os.remove(filename)
-    print("text spa: %s \ntext spa.news-gothic: %s \ntext eng: %s\n" % (text_default, text, text_eng))
     cv2.imshow("group threshed" + str(i), group)
     cv2.waitKey(0)
 
 
+    """
+    Each letter for every section
+    """
+    digitCnts = cv2.findContours(group.copy(), cv2.RETR_LIST,
+    	cv2.CHAIN_APPROX_SIMPLE)
+    digitCnts = digitCnts[0] if imutils.is_cv2() else digitCnts[1]
+
+    # Get Contours that could be letters based on size
+    actualLetters = []
+    for (index, possibleLetter) in enumerate(digitCnts):
+        (x, y, w, h) = cv2.boundingRect(possibleLetter)
+        if (h > 5):
+            actualLetters.append(possibleLetter)
+
+    digitCnts = contours.sort_contours(actualLetters,
+    	method="left-to-right")[0]
+
+    # Try to match the letter to the template.
+    for c in digitCnts:
+        # Bounding Rectangle of an area found in the ID.
+        (x, y, w, h) = cv2.boundingRect(c)
+        roi = group[y:y + h, x:x + w]
+        #cv2.imshow("roi :" + str(c), roi)
+        #cv2.waitKey(0)
+
+        roi = cv2.resize(roi, (57, 88))
+
+        #cv2.imshow("roi :" + str(c), roi)
+        #cv2.waitKey(0)
+
+        # initialize a list of template matching scores
+        scores = []
+
+
+        # loop over the reference name and ROI
+        for (digit, digitROI) in digits.items():
+        	# apply correlation-based template matching, take the
+        	# score, and update the scores list
+        	result = cv2.matchTemplate(roi, digitROI,
+        		cv2.TM_CCOEFF)
+        	(_, score, _, _) = cv2.minMaxLoc(result)
+        	scores.append(score)
+
+        # the classification for the digit ROI will be the reference
+        # digit name with the *largest* template matching score
+        groupOutput.append(str(np.argmax(scores)))
+    """END
+    Each letter for every section
+    """
+
+    # draw the digit classifications around the group
+    cv2.rectangle(image, (gX - 5, gY - 5),
+    	(gX + gW + 5, gY + gH + 5), (0, 0, 255), 2)
+    cv2.putText(image, "".join(groupOutput), (gX, gY - 15),
+    	cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+
+    # update the output digits list
+    output.extend(groupOutput)
+
+
 # display the output credit card information to the screen
-# print("OCR: {}".format("".join(output)))
+print("OCR: {}".format("".join(output)))
 cv2.imshow("Image", image)
 cv2.waitKey(0)
---
+++ ocr_transform.py
@@ -0,0 +1,182 @@
+from skimage.filters import threshold_local
+from transform import four_point_transform
+from imutils import contours
+from PIL import Image
+import pytesseract
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+
+def imWarped(image, debug=False):
+	# load the image and compute the ratio of the old height
+	# to the new height, clone it, and resize it
+	ratio = image.shape[0] / 500.0
+	orig = image.copy()
+	image = imutils.resize(image, height = 500)
+
+	# convert the image to grayscale, blur it, and find edges
+	# in the image
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 75, 200)
+
+	# show the original image and the edge detected image
+	if debug:
+		print("STEP 1: Edge Detection")
+		cv2.imshow("Image", image)
+		cv2.imshow("Edged", edged)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+	# find the contours in the edged image, keeping only the
+	# largest ones, and initialize the screen contour
+	_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+	# loop over the contours
+	for c in cnts:
+		# approximate the contour
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+
+		# if our approximated contour has four points, then we
+		# can assume that we have found our screen
+		if len(approx) == 4:
+			screenCnt = approx
+			break
+
+	# show the contour (outline) of the piece of paper
+	if debug:
+		print("STEP 2: Find contours of paper")
+		cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+		cv2.imshow("Outline", image)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+	# apply the four point transform to obtain a top-down
+	# view of the original image
+	warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+	# convert the warped image to grayscale, then threshold it
+	# to give it that 'black and white' paper effect
+	# warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+	# warped = threshold_local(warped, 251, offset = 10)
+	# warped = warped.astype("uint8") * 255
+
+	# show the original and scanned images
+	if debug:
+		print("STEP 3: Apply perspective transform")
+		cv2.destroyAllWindows()
+		cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+	return warped
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
+
+# initialize a rectangular (wider than it is tall) and square
+# structuring kernel
+rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread(args["image"])
+image = imWarped(image)
+image = imutils.resize(image, width=600)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray", gray)
+cv2.waitKey(0)
+
+# apply a tophat (whitehat) morphological operator to find light
+# regions against a dark background (i.e., the text areas)
+tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+cv2.imshow("Tophat", tophat)
+cv2.waitKey(0)
+
+# compute the Scharr gradient of the tophat image, then scale
+# the rest back into the range [0, 255]
+gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0,
+	ksize=-1)
+gradX = np.absolute(gradX)
+(minVal, maxVal) = (np.min(gradX), np.max(gradX))
+gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
+gradX = gradX.astype("uint8")
+
+
+# apply a closing operation using the rectangular kernel to help
+# cloes gaps in between letters, then apply
+# Otsu's thresholding method to binarize the image
+gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+thresh = cv2.threshold(gradX, 0, 255,
+	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
+
+
+# apply a second closing operation to the binary image, again
+# to help close gaps between credit card number regions
+# thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
+# cv2.imshow("Thresh-2", thresh)
+# cv2.waitKey(0)
+
+NAME_ROI = (186, 108, 255, 56)
+ADDR_ROI = (182, 179, 281, 61)
+CURP_ROI = (226, 265, 170, 20)
+BRDY_ROI = (487, 106, 81, 16)
+SEX_ROI = (524, 122, 64, 26)
+
+# Uncomment to recalculate ROIs from ID
+# fromCenter = False
+# NAME_ROI = cv2.selectROI("Image", tophat, fromCenter)
+# ADDR_ROI = cv2.selectROI("Image", tophat, fromCenter)
+# CURP_ROI = cv2.selectROI("Image", tophat, fromCenter)
+# BRDY_ROI = cv2.selectROI("Image", tophat, fromCenter)
+# SEX_ROI  = cv2.selectROI("Image", tophat, fromCenter)
+# print('NAME_ROI = ' + str(NAME_ROI))
+# print('ADDR_ROI = ' + str(ADDR_ROI))
+# print('CURP_ROI = ' + str(CURP_ROI))
+# print('BRDY_ROI = ' + str(BRDY_ROI))
+# print('SEX_ROI = ' + str(SEX_ROI))
+
+locs = [NAME_ROI, ADDR_ROI, CURP_ROI, BRDY_ROI, SEX_ROI]
+locs = sorted(locs, key=lambda x:x[1])
+"""
+Each rectangle (area of text)
+"""
+for (i, (gX, gY, gW, gH)) in enumerate(locs):
+    # initialize the list of group digits
+    groupOutput = []
+
+    # Extract the rectangle from the gray image
+    group = gray[gY - 5:gY + gH + 5, gX - 5:gX + gW + 5]
+    #cv2.imshow("group" + str(i), group)
+    #cv2.waitKey(0)
+    group = cv2.threshold(group, 0, 255,
+    	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # write the image to disk as a temporary file so we can
+    # apply OCR to it
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, group)
+
+    # load the image as a PIL/Pillow image, apply OCR, and then delete
+    # the temporary file
+    text = pytesseract.image_to_string(Image.open(filename), lang='spa.news-gothic-light')
+    text_default = pytesseract.image_to_string(Image.open(filename), lang='spa')
+    text_eng = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    print("text spa: %s \ntext spa.news-gothic: %s \ntext eng: %s\n" % (text_default, text, text_eng))
+    cv2.imshow("group threshed" + str(i), group)
+    cv2.waitKey(0)
+
+
+# display the output credit card information to the screen
+# print("OCR: {}".format("".join(output)))
+cv2.imshow("Image", image)
+cv2.waitKey(0)
